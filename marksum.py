de58632d2bb0c8b5a0f429f40018d6505298f5ab
Adam Starr <adamstarr33@gmail.com>
2021-01-15 13:18:03 -0500

    Oh hi mark

--- marksum.py
+++ marksum.py
@@ -1,22 +1,22 @@
 import random
 def marksum(n):
 
-    a = random.randint(0, 1000000)
+    a = random.randint(0, 100000000)
     list = []
     if n == 1:
         return 1
 
     if n == 2:
-        list.append(a/1000000)
-        list.append((1000000-a)/1000000)
+        list.append(a/100000000)
+        list.append((100000000-a)/100000000)
         return list
 
-    list.append(a/1000000)
+    list.append(a/100000000)
     temp = a
     for x in range(0,n-2):
-        mark = random.randint(0, 1000000-temp)
-        list.append(mark/1000000)
-        temp = mark
+        mark = random.randint(0, 100000000-temp)
+        list.append(mark/100000000)
+        temp = temp + mark
     list.append(1-sum(list))
     return list
 # list = []
@@ -24,4 +24,12 @@ def marksum(n):
 # for x in range(0, n-1):
 #     temp = random.randint(0, 1000000)
 n = int(input("Oh hi Mark! Please input your desired N value: "))
-print(marksum(n))
+res = marksum(n)
+count = 0
+for x in res:
+    count+=1
+    print(x)
+    if count != len(res):
+        print("+")
+print("=")
+print(sum(res))
